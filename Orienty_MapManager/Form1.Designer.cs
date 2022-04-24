06be5bc1027de635ee8a38b621a7a9b6d14c337e
VanillaFury <83728532+VanillaFury@users.noreply.github.com>
2022-04-24 22:04:07 +0300

    Микроправки дизайна

--- Orienty_MapManager/Form1.Designer.cs
+++ Orienty_MapManager/Form1.Designer.cs
@@ -36,16 +36,20 @@
             this.sheet = new System.Windows.Forms.PictureBox();
             this.saveButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
+            this.panelRight.SuspendLayout();
+            this.panelLeft.SuspendLayout();
             this.SuspendLayout();
             // 
             // selectButton
             // 
             this.selectButton.Image = global::Orienty_MapManager.Properties.Resources.cursor;
-            this.selectButton.Location = new System.Drawing.Point(24, 23);
-            this.selectButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.selectButton.Location = new System.Drawing.Point(15, 15);
+            this.selectButton.Margin = new System.Windows.Forms.Padding(6);
             this.selectButton.Name = "selectButton";
-            this.selectButton.Size = new System.Drawing.Size(90, 87);
+            this.selectButton.Size = new System.Drawing.Size(100, 100);
             this.selectButton.TabIndex = 9;
             this.selectButton.UseVisualStyleBackColor = true;
             this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
@@ -53,10 +57,10 @@
             // deleteALLButton
             // 
             this.deleteALLButton.Image = global::Orienty_MapManager.Properties.Resources.deleteAll;
-            this.deleteALLButton.Location = new System.Drawing.Point(26, 417);
-            this.deleteALLButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.deleteALLButton.Location = new System.Drawing.Point(127, 239);
+            this.deleteALLButton.Margin = new System.Windows.Forms.Padding(6);
             this.deleteALLButton.Name = "deleteALLButton";
-            this.deleteALLButton.Size = new System.Drawing.Size(90, 87);
+            this.deleteALLButton.Size = new System.Drawing.Size(100, 100);
             this.deleteALLButton.TabIndex = 5;
             this.deleteALLButton.UseVisualStyleBackColor = true;
             this.deleteALLButton.Click += new System.EventHandler(this.deleteALLButton_Click);
@@ -64,10 +68,10 @@
             // deleteButton
             // 
             this.deleteButton.Image = global::Orienty_MapManager.Properties.Resources.delete;
-            this.deleteButton.Location = new System.Drawing.Point(26, 319);
-            this.deleteButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.deleteButton.Location = new System.Drawing.Point(15, 239);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(6);
             this.deleteButton.Name = "deleteButton";
-            this.deleteButton.Size = new System.Drawing.Size(90, 87);
+            this.deleteButton.Size = new System.Drawing.Size(100, 100);
             this.deleteButton.TabIndex = 3;
             this.deleteButton.UseVisualStyleBackColor = true;
             this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
@@ -75,10 +79,10 @@
             // drawEdgeButton
             // 
             this.drawEdgeButton.Image = global::Orienty_MapManager.Properties.Resources.edge;
-            this.drawEdgeButton.Location = new System.Drawing.Point(24, 221);
-            this.drawEdgeButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.drawEdgeButton.Location = new System.Drawing.Point(127, 127);
+            this.drawEdgeButton.Margin = new System.Windows.Forms.Padding(6);
             this.drawEdgeButton.Name = "drawEdgeButton";
-            this.drawEdgeButton.Size = new System.Drawing.Size(90, 87);
+            this.drawEdgeButton.Size = new System.Drawing.Size(100, 100);
             this.drawEdgeButton.TabIndex = 2;
             this.drawEdgeButton.UseVisualStyleBackColor = true;
             this.drawEdgeButton.Click += new System.EventHandler(this.drawEdgeButton_Click);
@@ -86,10 +90,10 @@
             // drawVertexButton
             // 
             this.drawVertexButton.Image = global::Orienty_MapManager.Properties.Resources.vertex;
-            this.drawVertexButton.Location = new System.Drawing.Point(26, 123);
-            this.drawVertexButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.drawVertexButton.Location = new System.Drawing.Point(15, 127);
+            this.drawVertexButton.Margin = new System.Windows.Forms.Padding(6);
             this.drawVertexButton.Name = "drawVertexButton";
-            this.drawVertexButton.Size = new System.Drawing.Size(90, 87);
+            this.drawVertexButton.Size = new System.Drawing.Size(100, 100);
             this.drawVertexButton.TabIndex = 1;
             this.drawVertexButton.UseVisualStyleBackColor = true;
             this.drawVertexButton.Click += new System.EventHandler(this.drawVertexButton_Click);
@@ -97,50 +101,77 @@
             // sheet
             // 
             this.sheet.BackColor = System.Drawing.SystemColors.Control;
-            this.sheet.Location = new System.Drawing.Point(140, 23);
-            this.sheet.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.sheet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sheet.Location = new System.Drawing.Point(233, 0);
+            this.sheet.Margin = new System.Windows.Forms.Padding(6);
             this.sheet.Name = "sheet";
-            this.sheet.Size = new System.Drawing.Size(1268, 798);
+            this.sheet.Size = new System.Drawing.Size(796, 854);
             this.sheet.TabIndex = 0;
             this.sheet.TabStop = false;
             this.sheet.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sheet_MouseClick);
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(1584, 787);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(25, 792);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(15);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(192, 44);
+            this.saveButton.Size = new System.Drawing.Size(182, 44);
             this.saveButton.TabIndex = 13;
-            this.saveButton.Text = "Сохранить граф";
+            this.saveButton.Text = "Сохранить";
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1426, 138);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(23, 463);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(500, 590);
+            this.textBox1.Size = new System.Drawing.Size(368, 379);
             this.textBox1.TabIndex = 14;
+            // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.textBox1);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(1029, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(403, 854);
+            this.panelRight.TabIndex = 15;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.AutoSize = true;
+            this.panelLeft.BackColor = System.Drawing.SystemColors.Control;
+            this.panelLeft.Controls.Add(this.drawVertexButton);
+            this.panelLeft.Controls.Add(this.drawEdgeButton);
+            this.panelLeft.Controls.Add(this.deleteButton);
+            this.panelLeft.Controls.Add(this.saveButton);
+            this.panelLeft.Controls.Add(this.deleteALLButton);
+            this.panelLeft.Controls.Add(this.selectButton);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(233, 854);
+            this.panelLeft.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1938, 854);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.selectButton);
-            this.Controls.Add(this.deleteALLButton);
-            this.Controls.Add(this.deleteButton);
-            this.Controls.Add(this.drawEdgeButton);
-            this.Controls.Add(this.drawVertexButton);
+            this.ClientSize = new System.Drawing.Size(1432, 854);
             this.Controls.Add(this.sheet);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelRight);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "vscode.ru";
             ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
+            this.panelLeft.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,6 +187,8 @@
         private System.Windows.Forms.Button selectButton;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel panelLeft;
     }
 }
 
