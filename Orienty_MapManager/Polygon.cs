8b8d6436a80d24e9a26b1a027600525ccaba7f83
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2022-04-27 12:03:16 +0300

    test commit

--- Orienty_MapManager/Polygon.cs
+++ Orienty_MapManager/Polygon.cs
@@ -73,6 +73,68 @@ namespace Orienty_MapManager
 
         }
 
+        public static Point Intersection(Point A, Point B, Point C, Point D)
+        {
+            double xo = A.X, yo = A.Y;
+            double p = B.X - A.X, q = B.Y - A.Y;
+
+            double x1 = C.X, y1 = C.Y;
+            double p1 = D.X - C.X, q1 = D.Y - C.Y;
+
+            double x = (xo * q * p1 - x1 * q1 * p - yo * p * p1 + y1 * p * p1) /
+                (q * p1 - q1 * p);
+            double y = (yo * p * q1 - y1 * p1 * q - xo * q * q1 + x1 * q * q1) /
+                (p * q1 - p1 * q);
+
+            if (Double.IsNaN(x))
+                x = int.MaxValue-1;
+            if (Double.IsNaN(y))
+                y = int.MaxValue - 1;
+
+            return new Point(Convert.ToInt32(x), Convert.ToInt32(y));
+        }
+
+        public static bool IsBelongLine(Point a1, Point a2, Point b)
+        {
+            double x1 = a1.X;
+            double y1 = a1.Y;
+            double x2 = a2.X;
+            double y2 = a2.Y;
+            double x3 = b.X;
+            double y3 = b.Y;
+            /*return (Math.Abs((x3 - x1) * (y2 - y1) - (y3 - y1) * (x2 - x1)) <= 0.1) ||
+                ((x1 < x3 && x3 < x2) || (x2 < x3 && x3 < x1));*/
+            return (Math.Abs(x1 - x3) / Math.Abs(y1 - y3) - Math.Abs(x2 - x3) / Math.Abs(y2 - y3) >= 0.001);
+            /*double a= (y1 - y2) / (x1 - x2);
+            double c= ((y1 + y2) - a * (x1 + x2)) / 2;
+            if ((y3 == a * x3 + c) && (x3 > x1) && (x3 < x2) || (y3 == a * x3 + c) && (x2 > x1) && (x3 < x1))
+                return true;
+            else
+                returnn false;*/
+            //return (Math.Abs(Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2)) + Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2)) - Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))) < 0.1) &&
+
+        }
+
+        public static Point GetCorrectPoint(Point mouse, Polygon build, Point lastPoint)
+        {
+            Point pointPer;
+            for (int i = 0; i < build.points.Count-1; i++)
+            {
+                pointPer = Intersection(build.points[i], build.points[i + 1], mouse, lastPoint);
+                if(IsBelongLine(mouse, lastPoint, pointPer))
+                {
+                    return pointPer;
+                }
+                
+            }
+            pointPer = Intersection(build.points[0], build.points[build.points.Count-1], mouse, lastPoint);
+            if (IsBelongLine(mouse, lastPoint, pointPer))
+            {
+                return pointPer;
+            }
+            return mouse;
+        }
+
         private int GetDistanceBetweenPoints(Point first, Point second)
         {
             int result = (second.X - first.X) * (second.X - first.X);
